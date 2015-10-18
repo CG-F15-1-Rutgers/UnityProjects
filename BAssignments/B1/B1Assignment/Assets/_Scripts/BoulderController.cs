f6b29b74771c0c0d32d64760d835ca016fa57fcb
tolbyflew <tolbyflew@gmail.com>
2015-10-18 01:57:33 -0400

    OBSTACLES NOW A GO! Only the obstacle you click will move and will stop responding to arrow keys when other things are clicked

---
+++ BAssignments/B1/B1Assignment/Assets/_Scripts/BoulderController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderController : MonoBehaviour {
+    public Transform objectHit;
+    public Camera camera;
+    public float speed;
+    bool clicked;
+
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        clicked = false;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            objectHit = hit.transform;
+            print(clicked);
+            if (Input.GetMouseButtonDown(0) && clicked == false)
+            {
+
+                print("HERE");
+                if (objectHit.transform.gameObject.Equals(this.gameObject)) {
+                    //print(objectHit.transform.gameObject.name + " GO GO GO!");
+                    //print(this.gameObject.name);
+                    objectHit = hit.transform;
+                    clicked = true;
+                }
+                    
+            
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && clicked == true && !objectHit.transform.gameObject.Equals(this.gameObject))
+        {
+            print("UNEQUIPPED!");
+            clicked = false;
+        }
+        if (clicked)
+        {
+            float moveHorizontal = 0;
+            float moveVertical = 0;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveHorizontal = -1;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveHorizontal = 1;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                moveVertical = 1;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                moveVertical = -1;
+            }
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            print(movement);
+            rb.AddForce(movement * speed);
+            
+        }
+    }
+}
---
+++ BAssignments/B1/B1Assignment/Assets/_Scripts/ObstacleControl.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleControl : MonoBehaviour {
+
+    public Transform objectHit;
+    public Camera camera;
+    public float speed;
+    bool clicked;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+        clicked = false;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                clicked = !clicked;
+            }
+                objectHit = hit.transform;
+            
+                if (objectHit.tag.ToString().Equals("Obstacle"))
+                {
+                    print("here we go!");
+                    rb = objectHit.transform.gameObject.GetComponent<Rigidbody>();
+                    float moveHorizontal = 0;
+                    float moveVertical = 0;
+                    if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        print("left");
+                        moveHorizontal = -1;
+                    }
+                    else if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        moveHorizontal = 1;
+                    }
+
+                    if (Input.GetKey(KeyCode.UpArrow))
+                    {
+                        moveVertical = 1;
+                    }
+                    else if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        moveVertical = -1;
+                    }
+                    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+                    //rb = GetComponent<Rigidbody>();
+                    //rb.AddForce(movement * speed);
+                    print(movement);
+                    rb.AddForce(movement * speed);
+                
+            }
+        }
+        
+    }
+}
